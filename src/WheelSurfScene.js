30610d0d0ece875d2f62e07fff4aa4c264bf625e
yanli.huang <yanli.huang@chukong-inc.com>
2016-01-15 16:00:10 +0800

    优化

--- src/WheelSurfScene.js
+++ src/WheelSurfScene.js
@@ -10,7 +10,9 @@ var WheelSurfLayer = cc.Layer.extend({
     wheelEmpty:null,
     targetPoint:null,
     startBtn:null,
-    chooseNum:null,
+    chooseNum:0,
+    lastNum: 0,
+    totalNum:14,
     isEnabledClicked:null,
     ctor:function () {
         this._super();
@@ -39,21 +41,21 @@ var WheelSurfLayer = cc.Layer.extend({
         // add the label as a child to this layer
         this.addChild(lotteryLabel, 3);
 
-        var wheelBg = new cc.Sprite(res.WheelBackground_png)
+        var wheelBg = new cc.Sprite(res.WheelBackground_png);
         wheelBg.attr({
            x:size.width / 2,
            y:size.height / 2
         });
         this.addChild(wheelBg, 1);
 
-        this.wheelEmpty = new cc.Sprite(res.WheelEmpty_png)
+        this.wheelEmpty = new cc.Sprite(res.WheelEmpty_png);
         this.wheelEmpty.attr({
             x:size.width / 2,
             y:size.height / 2
         });
         this.addChild(this.wheelEmpty, 1);
 
-        var startBg = new cc.Sprite(res.StartBackground_png)
+        var startBg = new cc.Sprite(res.StartBackground_png);
         startBg.attr({
             x:size.width / 2,
             y:size.height / 2
@@ -96,17 +98,17 @@ var WheelSurfLayer = cc.Layer.extend({
 
         vecPos = new Array();
 
-        for(var i = 10; i < 20; i++)
+        for(var i = this.totalNum; i < this.totalNum * 2; i++)
         {
-            var radians = cc.degreesToRadians(36*(i));
+            var radians = cc.degreesToRadians(360/this.totalNum*(i));
             vecPos.push(cc.p(radius*Math.sin(radians),radius*Math.cos(radians)))
         }
-        for(var i = 10; i < 20; i++)
+        for(var i = this.totalNum; i < this.totalNum * 2; i++)
         {
-            vecPos[i] = vecPos[i - 10];
+            vecPos[i] = vecPos[i - this.totalNum];
         }
 
-        for (var i=0;i<10;i++)
+        for (var i=0;i<this.totalNum;i++)
         {
             var frame = new cc.Sprite(res.ItemBackground_png);
             frame.setPosition(vecPos[i]);
@@ -116,9 +118,9 @@ var WheelSurfLayer = cc.Layer.extend({
         this.targetPoint.setPosition(vecPos[0]);
         this.originalPoint.addChild(this.targetPoint, 7);
 
-        var vecName = ["李协成", "王振璜", "陈燕淑", "孟丹丹", "陈峰", "王必杰", "李志艺", "游月传", "李播", "苏晓超"];
+        var vecName = ["李播", "苏晓超","王振璜","王必杰", "李志艺", "游月传", "陈燕淑", "孟丹丹", "陈峰", "黄艳丽","李协成","张煜轩","陈圆圆", "刘妍"];
 
-        for(var i = 0; i < 10; i++)
+        for(var i = 0; i < this.totalNum; i++)
         {
             var item = new cc.LabelTTF(vecName[i], "Arial", 25);
             item.setPosition(vecPos[i]);
@@ -134,20 +136,19 @@ var WheelSurfLayer = cc.Layer.extend({
             this.targetPoint.setPosition(vecPos[index]);
         }
     },
-    getBaseSequenceActionWithcircleId:function(index){
+    getBaseSequenceActionWithcircleId:function(index, total){
 
         var delayTime = 0.1;
-        if(index >0)
+        var actionArray = new Array();
+        for (var i = index;i < index + total; i++)
         {
-            var actionArray = new Array();
-            for (var i = 0;i < index; i++)
-            {
-                actionArray.push(cc.delayTime(delayTime));
-                actionArray.push(cc.callFunc(this.setFramePosition,this,i));
-            }
-            var rotateSequence1 = cc.sequence(actionArray);
-            return rotateSequence1;
+            var num = i > this.totalNum ? i - this.totalNum :i;
+            cc.log("num" + num);
+            actionArray.push(cc.delayTime(delayTime));
+            actionArray.push(cc.callFunc(this.setFramePosition,this,num));
         }
+        var rotateSequence1 = cc.sequence(actionArray);
+        return rotateSequence1;
 
     },
     getPlayCheckedEffectAction:function(index){
@@ -156,16 +157,20 @@ var WheelSurfLayer = cc.Layer.extend({
     onStartCallback:function(sender){
         if(this.isEnabledClicked)
         {
+            this.lastNum = this.lastNum + this.chooseNum;
+            this.lastNum = this.lastNum > this.totalNum ?this.lastNum -  this.totalNum :this.lastNum;
             this.isEnabledClicked = false;
-            chooseNum = Math.ceil(Math.random() * 10)
+            var num = Math.random()* this.totalNum + 1;
+            this.chooseNum = parseInt(num, 10);
+            cc.log("chooseNum" + this.chooseNum);
             var callback = cc.callFunc(this.onEndCallback, this);
             var rotateSequence = cc.sequence(
-                this.getBaseSequenceActionWithcircleId(10),
-                this.getBaseSequenceActionWithcircleId(10),
-                this.getBaseSequenceActionWithcircleId(10),
-                this.getBaseSequenceActionWithcircleId(10),
-                this.getBaseSequenceActionWithcircleId(10),
-                this.getBaseSequenceActionWithcircleId(chooseNum),
+                this.getBaseSequenceActionWithcircleId(this.lastNum,this.totalNum),
+                this.getBaseSequenceActionWithcircleId(this.lastNum,this.totalNum),
+                this.getBaseSequenceActionWithcircleId(this.lastNum,this.totalNum),
+                this.getBaseSequenceActionWithcircleId(this.lastNum,this.totalNum),
+                this.getBaseSequenceActionWithcircleId(this.lastNum,this.totalNum),
+                this.getBaseSequenceActionWithcircleId(this.lastNum,this.chooseNum),
                 callback
             );
 
